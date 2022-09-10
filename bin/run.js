1155a0a2a804171de56a436e0589fd682031e4a6
Ted Benson <ted@steamship.com>
2022-09-10 13:08:23 -0400

    Trying index naming convention

--- bin/run.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-// The purpose of the try-steamship NPM package is to invoke the `ship try` command
-// with no setup.
-process.argv.splice(2, 0, 'try')
-
-require('@steamship/cli').run()
-.then(require('@oclif/command/flush'))
-.catch(require('@oclif/errors/handle'))
